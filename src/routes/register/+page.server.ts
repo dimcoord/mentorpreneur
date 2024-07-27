d13d400d0b9a972ce3d907fcac8eb2910abf0792
dimcoord <dimastio20062@gmail.com>
2024-07-27 22:26:13 +0700

    databaes

--- src/routes/register/+page.server.ts
+++ src/routes/register/+page.server.ts
@@ -1,5 +1,41 @@
 import type { PageServerLoad } from './$types';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 export const load = (async () => {
 	return {};
 }) satisfies PageServerLoad;
+
+/** @type {import('./$types').Actions} */
+export const actions = {
+	register: async ({request}) => {
+		const data = await request.formData();
+		await prisma.user.create({
+			data: {
+			  email: data.get('email') as string,
+			  password: data.get('password') as string,
+			  name: data.get('name') as string,
+			  bio: data.get('bio') as string,
+			//   Experience: {
+			// 	create: {
+			// 		company: data.get('experience-company') as string,
+			// 		position: data.get('experience-position') as string
+			// 	}
+			//   },
+			//   Category: {
+			// 	create: {
+			// 		industry: [data.get('industry') as string],
+			// 		discipline: [data.get('discipline') as string],
+			// 		expertise: [data.get('expertise') as string]
+			// 	}
+			//   },
+			//   Schedule: {
+			// 	create: {
+			// 		topic: data.get('schedule-topic') as string
+			// 	}
+			//   }
+			},
+		  });
+	}
+}
--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -1,5 +1,42 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 
+
 	export let data: PageData;
 </script>
+
+<form method="POST">
+	<label>
+		email:
+		<input
+			name="email"
+			type="email"
+			autocomplete="off"
+		/>
+	</label>
+	<label>
+		password:
+		<input
+			name="password"
+			type="password"
+			autocomplete="off"
+		/>
+	</label>
+	<label>
+		name:
+		<input
+			name="name"
+			type="name"
+			autocomplete="off"
+		/>
+	</label>
+	<label>
+		bio:
+		<input
+			name="bio"
+			type="text"
+			autocomplete="off"
+		/>
+	</label>
+	<button>Submit</button>
+</form>
