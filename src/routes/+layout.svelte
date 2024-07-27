db34958efde4b29e28e758739eabfd9498b033e4
Andira Rahman <andira.a2n@gmail.com>
2024-07-27 18:49:38 +0700

    feat: Adds homepage front-end

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,12 +2,18 @@
 	// import type { LayoutData } from './$types';
 	import '../app.css';
 	import { Toaster } from '$lib/components/ui/sonner';
+	import { Button } from '$lib/components/ui/button';
 
 	// export let data: LayoutData;
 </script>
 
 <Toaster />
 
-<div class=" grid grid-cols-12 font-sans mx-4">
+<div class="font-sans mx-2 md:mx-4 my-2 grid grid-cols-12">
+	<div class="col-span-12 flex gap-1 md:gap-2 justify-end">
+		<Button size="sm" href="/register" class="text-xs">Register</Button>
+		<Button size="sm" href="/login" class="text-xs">Login</Button>
+		<Button size="sm" href="/about" class="text-xs">About Us</Button>
+	</div>
 	<slot />
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,14 @@
-<div class="col-span-12">
-	<h1 class="font-extrabold text-3xl">Mentorpreneur</h1>
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+</script>
+
+<div class="col-span-12 h-[90dvh] flex flex-col justify-between">
+	<div class="min-h-[75dvh] flex flex-col justify-center gap-4">
+		<h1 class="font-bold text-4xl md:text-6xl">Mentorpreneur</h1>
+		<p class="text-lg md:text-2xl">Connect. Mentor. Grow.</p>
+	</div>
+	<div class="flex flex-col items-start gap-2">
+		<Button href="/mentors" class="md:text-lg md:px-8 md:py-6 w-fit">Find Mentors</Button>
+		<Button variant="link" size="sm" href="/login" class="col-start-1 px-2">I'm a mentor</Button>
+	</div>
 </div>
